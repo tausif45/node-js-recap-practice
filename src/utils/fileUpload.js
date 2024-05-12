a07add3b8730afe0213a2e541ef2888f9d6c4e5f
tausif45 <tausif.dev45@gmail.com>
2024-05-13 00:31:19 +0530

    Initialisation of register api

---
+++ src/utils/fileUpload.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadFile = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log('File uploaded successfully', res.url)
+        return res;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export { uploadFile }
